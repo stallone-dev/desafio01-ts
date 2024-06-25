8dd515a27922b6bb41df54a441dbbf26f1c68500
stallone-dev <stallone.developer@gmail.com>
2024-06-24 23:24:47 -0300

    feat(CompanyAccount model): Implements 'getLoan' method

--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -1,12 +1,25 @@
-import { DioAccount } from "./DioAccount"
+import { DioAccount } from "./model/DioAccount"
 
-export class CompanyAccount extends DioAccount {
+export { CompanyAccount };
+
+class CompanyAccount extends DioAccount {
 
   constructor(name: string, accountNumber: number){
     super(name, accountNumber)
   }
 
-  getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
+  public getLoan(loan:number): void {
+    if(!this.validateStatus()){
+        console.warn("Your account is invalid!");
+        return;
+    };
+
+    console.log("Loan application requested.");
+    console.log("\tLoan under analysis.");
+    console.log("\tLoan approved!");
+    console.log(`the amount [$ ${loan}] will be deposited into your account.`);
+
+    this.deposit(loan);
+
   }
 }
